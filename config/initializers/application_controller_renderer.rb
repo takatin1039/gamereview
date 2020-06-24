519822ba4974cf914acf7a85317d9329d73e4e14
takatin1039 <kinoshita.takayuki.infratop@gmail.com>
2020-06-24 18:12:33 +0900

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'dabbea0f56cea9d720102c094924a36562eca2a2e898e5360be6ba4a39a2af79ac745490d277ec4458e2aa67ff432ef3b6dc4d31bfb92d5518295ade4c67bed7'
---
+++ db/migrate/20200410131316_add_rate_to_post_images.rb
@@ -0,0 +1,5 @@
+class AddRateToPostImages < ActiveRecord::Migration[5.2]
+  def change
+    add_column :post_images, :rate, :float
+  end
+end
